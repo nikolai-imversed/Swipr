22aa2243f23831a4b7493e6efafc8a6e122b9a83
nikolai-imversed <83116791+Niko-est@users.noreply.github.com>
2023-03-23 17:22:40 +0300

    Initial commit

---
+++ commands/ping.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+// module.exports = {
+//   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+//   async execute(interaction) {
+//     await interaction.reply({ content: 'Pong!', ephemeral: true });
+//   }
+// };
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
